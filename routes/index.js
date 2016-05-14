0ca69e5d8231c7228abda59bf59d451eca6f32ac
DeusExMachina <deusxmachina96@gmail.com>
2016-05-14 19:33:08 +0200

    Add pg-promise and connect to url_shortener_db

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var pgp = require('pg-promise')();
 var router = express.Router();
+var connectionOptions = {
+  host: 'localhost',
+  port: 5432,
+  database: 'url_shortener_db',
+  user: 'postgres',
+  password: 'postgres'
+};
 
+var db = pgp(connectionOptions);
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // Initial test to confirm connection to database
+
+  /*
+  db.any("SELECT * FROM url_short;")
+      .then(function(data) {
+        console.log("DATA: ", data);
+      })
+      .catch(function(error){
+        console.log("ERROR: ", error);
+      });
+   */
   res.render('index', { title: 'Express' });
 });
 
